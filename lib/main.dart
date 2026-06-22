a3fa80c32e73c1c7223ff7ea107d40e106f24031
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-22 08:15:02 +0100

    fix: async main + runZonedGuarded + github.init()

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-  import 'services/github_service.dart';
-  import 'screens/shell_screen.dart';
-  import 'theme.dart';
+import 'services/github_service.dart';
+import 'screens/shell_screen.dart';
+import 'theme.dart';
 
-  void main() { WidgetsFlutterBinding.ensureInitialized(); runApp(const AgentBaseApp()); }
+void main() {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final github = GitHubService();
+    await github.init();
+    runApp(AgentBaseApp(github: github));
+  }, (error, stack) {
+    debugPrint('AgentBase fatal: $error
+$stack');
+  });
+}
 
-  class AgentBaseApp extends StatelessWidget {
-    const AgentBaseApp({super.key});
-    @override
-    Widget build(BuildContext context) => MaterialApp(
-      title: 'AgentBase', theme: buildTheme(), debugShowCheckedModeBanner: false,
-      home: ShellScreen(github: GitHubService()),
-    );
-  }
+class AgentBaseApp extends StatelessWidget {
+  final GitHubService github;
+  const AgentBaseApp({super.key, required this.github});
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'AgentBase',
+    theme: buildTheme(),
+    debugShowCheckedModeBanner: false,
+    home: ShellScreen(github: github),
+  );
+}
